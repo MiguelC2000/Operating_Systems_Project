1e547d34e9ec43a3c6ca949dd11ec4d70fd30c8d
Miguel Correia <77934656+MiguelC2000@users.noreply.github.com>
2023-06-19 01:31:10 +0100

    Add files via upload

--- src/objectiveD.h
+++ src/objectiveD.h
@@ -2,15 +2,18 @@
 // Created by Bruno Miguel on 29/04/2022.
 //
 
-#ifndef SO_PROJETO_OBJECTIVED_H
-#define SO_PROJETO_OBJECTIVED_H
-#include "signal.h"
-#include "myData.h"
-#include "myDataTimeCount.h"
+#ifndef CHILD_OBJECTIVED_H
+#define CHILD_OBJECTIVED_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include "./myDataChild.h"
 #include "sys/socket.h"
 #include "sys/un.h"
-#include "unistd.h"
-
 #define BUF_SIZE 8192
-void socketServer();
-#endif //SO_PROJETO_OBJECTIVED_H
+
+void socketChild(DATA *myData, int i, int lines, int jump, char *env[], char roomName[4][MAX_BUFFER]);
+
+#endif //CHILD_OBJECTIVED_H
